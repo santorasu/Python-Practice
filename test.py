47c6881b485f9bddfaac8cc35baf45810cf4c553
Rashedul Islam <santorasu09@gmail.com>
2023-01-15 00:18:55 +0600

    This is day1 task

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 print("The world under attack by python")
 print(5+5)
+print("We are not same bro")
+print("Oil your own machine")
+#if else statement
+if 5>2:
+     print("Five is greater than two")
 # This is single line comment
 # djjffdjf
-# fdfdjf
+# fdfdjfzz
+a=2
+print(a)
+b=3.3
+print(b)
+c="String"
+print(c)
+print(a+b)
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
+d=complex(34, 33)
+print("type of d is ",type(d))
+e=True
+print("The type of d is ",type(d))
