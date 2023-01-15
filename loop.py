76e28bf32d053d70566e1cb5c34293b80a139e87
Rashedul Islam <santorasu09@gmail.com>
2023-01-15 19:13:40 +0600

    This is day13

---
+++ loop.py
@@ -0,0 +1,8 @@
+name="Santo Rasu"
+print(name[0:10])
+print(name[:10])
+print(name[2:10])
+print(name[:])
+print(name[:-4])
+print(name[-9:-1])
+print(len(name))
--- typecast.py
+++ typecast.py
@@ -1,3 +1,31 @@
-a="Santo"
-n="2"
-print(int(a)+int(n))
+# a="45"
+# n="2"
+# print(int(a)+int(n))
+name=input()
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+# print("My name is",name)
+# about=input("Enter your Ocupation: ")
+# print("I am a",about)
+# x=input("Enter first number: ")
+# y=input("Enter Second number: ")
+# print(x+y)
+# print(int(x)+int(y))
+
+apple= '''He said,"I want to 
+eat an apple
+this is another line '''
+print(apple)
+st= """ Hey I'm Rasu
+I'm a CSE Engineer
+I live in Ashulia
+This is python code"""
+print(st)
+print("Let's use for loop\n")
+for chracter in name:
+    print(chracter)
+for chracter in st:
+    print(chracter)
