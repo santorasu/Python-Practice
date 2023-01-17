430cd26730d95cbb06db56c6856a45e6c1ff1693
Rashedul Islam <santorasu09@gmail.com>
2023-01-17 23:56:57 +0600

    This is day14 task

---
+++ if-else.py
@@ -0,0 +1,31 @@
+# a=int(input("Enter your age: "))
+# print("Your age is: ",a)
+
+# if(a>18):
+#     print("You can drive")
+# else:
+#     print("You cant't drive")
+
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+# num=int(input("Enter the value of num: "))
+# if(num<0):
+#     print("Number is negative")
+# elif(num==0):
+#     print("Number is zero")
+# else:
+#     print("Number is positive")
+
+applePrize=10
+budget=200
+if(budget - applePrize>50):
+    print("You can buy Apple")
+elif(budget - applePrize>70):
+    print("Okay you can buy")
+else:
+    print("You cant't buy apple")
---
+++ nasted.py
@@ -0,0 +1,12 @@
+num=0
+if(num<0):
+    print("Negative")
+elif(num>0):
+    if(num<=10):
+        print("Number is between 1-10")
+    elif(num>10):
+        print("Number between 11-20")
+    else:
+        print("Number is greater then 20")   
+else:
+    print("Number is zero")
