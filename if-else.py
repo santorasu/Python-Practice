273c09a156f222ceb6314fbd882217c1408f738f
Rashedul Islam <santorasu09@gmail.com>
2023-01-18 19:08:52 +0600

    This is day15

--- if-else.py
+++ if-else.py
@@ -1,25 +1,25 @@
-# a=int(input("Enter your age: "))
-# print("Your age is: ",a)
+a=int(input("Enter your age: "))
+print("Your age is: ",a)
 
-# if(a>18):
-#     print("You can drive")
-# else:
-#     print("You cant't drive")
+if(a>18):
+    print("You can drive")
+else:
+    print("You cant't drive")
 
-# print(a>18)
-# print(a<18)
-# print(a>=18)
-# print(a<=18)
-# print(a==18)
-# print(a!=18)
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
 
-# num=int(input("Enter the value of num: "))
-# if(num<0):
-#     print("Number is negative")
-# elif(num==0):
-#     print("Number is zero")
-# else:
-#     print("Number is positive")
+num=int(input("Enter the value of num: "))
+if(num<0):
+    print("Number is negative")
+elif(num==0):
+    print("Number is zero")
+else:
+    print("Number is positive")
 
 applePrize=10
 budget=200
