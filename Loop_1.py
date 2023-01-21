1d9a68b54ace726696f02a5a5143b624d392eb96
Rashedul Islam <santorasu09@gmail.com>
2023-01-21 23:41:41 +0600

    This is day17 task

--- Loop_1.py
+++ Loop_1.py
@@ -3,6 +3,17 @@
 #     print(i)
 #     if(i=="a"):
 #         print("this is special")
-colors=["red","green","blue","Yellow"]
-for i in colors:
-    print(i)
+# colors = ["red", "green", "blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for k in range(5):
+#     print(k)
+# print("\n")
+# for k in range(1,100):
+#     print(k)
+
+for k in range(1,20,3):
+    print(k)
